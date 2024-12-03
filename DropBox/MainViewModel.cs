a698cc9e3daf9f451bcd50e666c6b5b24e7c5970
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-12-03 16:05:38 +0000

    Initial UI with dummy data

---
+++ DropBox/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace DropBox
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<string> _items;
+        public List<string> Items
+        {
+            get { return _items; }
+            set
+            {
+                _items = value;
+                OnPropertyChanged(nameof(Items));
+            }
+        }
+
+        public MainViewModel()
+        {
+            Items = new List<string> { "File1", "File2" };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DropBox/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DropBox
+{
+    public partial class MainWindow : Window
+    {
+        public MainViewModel ViewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            ViewModel = new MainViewModel();
+            ViewModel.Items = new List<string> { "One", "Two" };
+            this.DataContext = this;
+        }
+    }
+}
