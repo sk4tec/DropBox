40f13449db4324b976b8ad7a5da5c60b40316b60
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-12-04 11:29:27 +0000

    Add a Logging class
    Simple logging class Subscribe to LogCreated Event to be notified of events

---
+++ DropBox/Services/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DropBox.Services
+{
+    public class Logger : ILogger
+    {
+        private readonly List<string> _logEntries;
+
+        public Logger()
+        {
+            _logEntries = new List<string>();
+        }
+
+        public event EventHandler<string>? LogCreated;
+
+        public void Log(string message)
+        {
+            _logEntries.Add(message);
+            OnLogCreated(message);
+        }
+
+        public IEnumerable<string> GetLogEntries()
+        {
+            return _logEntries;
+        }
+
+        protected virtual void OnLogCreated(string logEntry)
+        {
+            LogCreated?.Invoke(this, logEntry);
+        }
+    }
+}
